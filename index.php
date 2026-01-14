0029c7bc70bfc2016c785d03db9e246df9af534f
STORMTR00P3R <noorblum16@gmail.com>
2026-01-13 16:26:41 -0800

    init

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Hermes RSS Reader</title>
+  <link rel="stylesheet" href="style.css">
+  <meta name="description" content="">
+
+  <meta property="og:title" content="Hermes RSS Reader">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="localhost">
+  <meta property="og:image" content="TODO">
+  <meta property="og:image:alt" content="TODO">
+
+</head>
+
+<body>
+
+  <!-- Add your site or application content here -->
+  <p>Hello world! This is HTML5 Boilerplate.</p>
+</body>
+
+</html>
